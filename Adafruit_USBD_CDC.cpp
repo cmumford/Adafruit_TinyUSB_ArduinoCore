1f3cb11b2e543e111eb90458075aec924ea0498b
Chris Mumford <cmumford@chromium.org>
2020-12-18 13:31:05 -0800

    Re-added Adafruit_TinyUSB_Core.h.

--- Adafruit_USBD_CDC.cpp
+++ Adafruit_USBD_CDC.cpp
@@ -161,10 +161,8 @@ void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts) {
     tud_cdc_get_line_coding(&coding);
 
     // Investigage purpose of this. Is this Arduino IDE specific?
-#if 0
     if (coding.bit_rate == 1200)
       Adafruit_TinyUSB_Core_touch1200();
-#endif
   }
 }
 }
